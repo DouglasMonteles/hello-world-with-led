f30ece8b2381f7b2c8bfc32fb594f12a97b52db3
douglasmonteles <silvadouglas328@gmail.com>
2023-02-08 23:18:18 -0300

    Send requests through mqtt connection

--- src/main.c
+++ src/main.c
@@ -1,46 +1,65 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-
-   Link: https://github.com/platformio/platform-espressif32/blob/master/examples/espidf-blink/src/blink.c
-*/
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
+#include <string.h>
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
 
-/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO 2
+#include "wifi.h"
+#include "mqtt.h"
 
-void app_main() {
-  /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-     muxed to GPIO on reset already, but some default to other
-     functions and need to be switched to GPIO. Consult the
-     Technical Reference for a list of pads and their default
-     functions.)
-  */
-  esp_rom_gpio_pad_select_gpio(BLINK_GPIO);
-  /* Set the GPIO as a push/pull output */
-  gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+SemaphoreHandle_t conexaoWifiSemaphore;
+SemaphoreHandle_t conexaoMQTTSemaphore;
 
-  printf("Hello World!\n");
-  fflush(stdout);
-
-  while (1) {
-    /* Blink off (output low) */
-    printf("Turning off the LED\n");
-    gpio_set_level(BLINK_GPIO, 0);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    /* Blink on (output high) */
-    printf("Turning on the LED\n");
-    gpio_set_level(BLINK_GPIO, 1);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+void conectadoWifi(void * params)
+{
+  while(true)
+  {
+    if(xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
+    {
+      // Processamento Internet
+      mqtt_start();
+    }
   }
+}
+
+void trataComunicacaoComServidor(void * params)
+{
+  char mensagem[50];
+  char json[200];
+
+  if(xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
+  {
+    while(true)
+    {
+      float temperatura = 20.0 + (float)rand()/(float)(RAND_MAX/10.0);
+      sprintf(mensagem, "{\"temperatura\": %f}", temperatura);
+      mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
+
+      sprintf(json, "{\"pinos\": 5}");
+      mqtt_envia_mensagem("v1/devices/me/attributes", json);
+
+      vTaskDelay(3000 / portTICK_PERIOD_MS);
+    }
+  }
+}
+
+void app_main(void)
+{
+    // Inicializa o NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+    conexaoMQTTSemaphore = xSemaphoreCreateBinary();
+    wifi_start();
+
+    xTaskCreate(&conectadoWifi,  "Conexão ao MQTT", 4096, NULL, 1, NULL);
+    xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker", 4096, NULL, 1, NULL);
 }
